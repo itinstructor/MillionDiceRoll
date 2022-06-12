c675f6b9cc2f8e41d1b4ed57b0a9dc8aab734d19
itinstructor <32806514+itinstructor@users.noreply.github.com>
2022-06-12 09:25:40 -0600

    Update Python version

--- README.md
+++ README.md
@@ -7,14 +7,15 @@ Create Million Dollar Dice Roll in the following languages:
 - C++
 - C#
 
-Time each program run to show the difference in speed between languages.
+Measure elapsed time to show the difference in speed between languages.
+
+## Changes
+- 06/12/2022: Initial Commit
 
 ## Credit    
 Adapted from: Million Dice Roll Statistics Simulator
 
 By Al Sweigart [www.inventwithpython.com](www.inventwithpython.com)
-
-A simulation of one million dice rolls.
 
 ### License
 <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
--- million_dice_roll_python.py
+++ million_dice_roll_python.py
@@ -1,44 +1,50 @@
 """
     Filename: million_dice_roll.py
+    Created: 06/12/22
     Adapted from: Million Dice Roll Statistics Simulator
     By Al Sweigart www.inventwithpython.com
-    A simulation of one million dice rolls.
+    Purpose: A simulation of one million dice rolls
 """
 import random
 import time
+print(" +--------------------------------------------+")
+print(" |-- Million Dice Roll Statistics Simulator --|")
+print(" +--------------------------------------------+")
 
-print("Million Dice Roll Statistics Simulator")
+number_of_dice = int(input(" How many dice do you want to roll: "))
 
-number_of_dice = int(input("Enter how many six-sided dice you want to roll: "))
-
-# Set up a dictionary to store the results of each dice roll:
+# Set up a dictionary to store the results of each dice roll
 results = {}
+# Fill dictionary keys with possible combinations of dice
+# Initialize the values to 0
 for i in range(number_of_dice, (number_of_dice * 6) + 1):
     results[i] = 0
 
-print(f'Simulating 1,000,000 rolls of {number_of_dice} dice...')
+print(f" Simulating 1,000,000 rolls of {number_of_dice} dice...")
 
-# Start calculation time
-start = time.time()
+# Record start calculation time
+start_time = time.time()
+
+# Roll the dice 1,000,000 times
 for i in range(1000000):
     total = 0
     for j in range(number_of_dice):
-        # total is the sum of all dice rolls
+        # total is the sum of all dice rolls of number_of_dice
         total = total + random.randint(1, 6)
-    # Add 1 to the result dictionary key
+    # Add 1 to the appropriate result dictionary key
     results[total] = results[total] + 1
 
-# End calculation time
-finish = time.time()
-# Calculate and display elapsed time in seconds
-elapsed_time = finish - start
-# Convert seconds to ms (milliseconds)
+# Record finish calculation time
+finish_time = time.time()
+# Calculate and display elapsed time in ms (milliseconds)
+elapsed_time = finish_time - start_time
+# Convert seconds to ms
 elapsed_time = elapsed_time * 1000
-print(f"Elapsed time: {round(elapsed_time)} ms")
+print(f" Elapsed time: {round(elapsed_time)} ms")
 
 # Display results
-print('TOTAL - ROLLS - PERCENTAGE')
+print(" Total  Rolls  Percentage")
 for i in range(number_of_dice, (number_of_dice * 6) + 1):
     roll = results[i]
     percentage = round(results[i] / 10000, 1)
-    print(f'  {i} - {roll} - {percentage}%')
+    print(f'{i:3} {roll:10,} {percentage:>6}%')
